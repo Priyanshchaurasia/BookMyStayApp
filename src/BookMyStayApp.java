1923eb59f09c45202b4b357a280b84910c4aff9a
Priyanshchaurasia <priyansh.chaurasia62@gmail.com>
2026-04-02 15:42:10 +0530

    Update BookMyStayApp.java

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,21 +1,100 @@
+// Version 2.1 - Refactored Room Initialization
+
+// Abstract Class
+abstract class Room {
+    private String roomType;
+    private int beds;
+    private double size;
+    private double price;
+
+    // Constructor
+    public Room(String roomType, int beds, double size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    // Getters (Encapsulation)
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Common method
+    public void displayDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+// Concrete Class - Single Room
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 120.0, 2000.0);
+    }
+}
+
+// Concrete Class - Double Room
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 200.0, 3500.0);
+    }
+}
+
+// Concrete Class - Suite Room
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 350.0, 6000.0);
+    }
+}
+
+// Main Class (Entry Point)
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madameee";
+        System.out.println("===== Book My Stay App (Version 2.1) =====\n");
 
-        // Reverse the string
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
-        }
+        // Polymorphism: Using Room reference
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        // Check palindrome
-        if (word.equals(reversed)) {
-            System.out.println("The string \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
-        }
+        // Static Availability Variables
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        // Display Details
+        System.out.println("---- Single Room ----");
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailability);
+        System.out.println();
+
+        System.out.println("---- Double Room ----");
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailability);
+        System.out.println();
+
+        System.out.println("---- Suite Room ----");
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailability);
+        System.out.println();
+
+        System.out.println("===== Application Terminated =====");
     }
 }
