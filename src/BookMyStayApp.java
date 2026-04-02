22c14bb6299f8f72cd3737e135e01802bfaf1532
priyansh-chaurasia <priyansh.chaurasia62@gmail.com>
2026-04-02 14:22:12 +0530

    UC2-COMPLETED

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,21 @@
 public class BookMyStayApp {
-    public static void main(String[] args)
-    {
-        System.out.println("Setup \n welcome");
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        // Check palindrome
+        if (word.equals(reversed)) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
     }
 }
